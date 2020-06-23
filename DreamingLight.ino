8c76aeb4e20e1f9944b27ff4a453baacd7dfa052
Pietro Prandini <prandinip@gmail.com>
2020-06-23 11:54:37 +0200

    README + code explained

--- DreamingLight.ino
+++ DreamingLight.ino
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Pietro Prandini
+ * Copyright 2020 Pietro Prandini.
  * 
  * This file is part of DreamingLight.
  *
@@ -17,73 +17,148 @@
  * along with DreamingLight.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const int buttonPin = 2;                  // the number of the button pin
-const int lightPin = 13;                  // the number of the light pin
-const int PUSHED = HIGH;                  // useful variable for understanding button
-const int NOT_PUSHED = LOW;               // useful variable for understanging button
-const int LIGHT_ON = HIGH;                // useful variable for understanging light states
-const int LIGHT_OFF = LOW;                // useful variable for understanging light states
-const unsigned long lightTime = 3600000;  // 1 hour of light time in milliseconds
+/**
+ * @brief The number of the button pin.
+ */
+const int BUTTON_PIN = 2;
 
-int lightState = LIGHT_OFF;       // the current state of the light pin
-int buttonState = NOT_PUSHED;     // the current reading from the button pin
-int buttonLastState = NOT_PUSHED; // the last button state
+/**
+ * @brief The number of the light pin.
+ */
+const int LIGHT_PIN = 13;
 
-unsigned long startLight = 0;     // variable for storing the time when the light starts
-unsigned long lastPush = 0;       // variable for storing the last push of the button
-unsigned long debounce = 200;     // the debounce time, increase if the output flickers (ms)
+/**
+ * @brief Useful variable for understanding button pushed state.
+ */
+const int PUSHED = HIGH;
 
+/**
+ * @brief Useful variable for understanding button NOT pushed state.
+ */
+const int NOT_PUSHED = LOW;
+
+/**
+ * @brief Useful variable for understanding light ON state.
+ */
+const int LIGHT_ON = HIGH;
+
+/**
+ * @brief Useful variable for understanding light OFF state.
+ */
+const int LIGHT_OFF = LOW;
+
+/**
+ * @brief Light ON default time in milliseconds.
+ * 
+ * Note: 1 hour is composed by 60 minutes,
+ * 1 minute is composed by 60 seconds,
+ * 1 seconds is 1000 milliseconds. 
+ */
+const unsigned long LIGHT_TIME_ms = 3600000;
+
+/**
+ * @brief The debounce time in milliseconds. 
+ * 
+ * Note: increase if the output flickers.
+ */
+const unsigned long DEBOUNCE_ms = 200;
+
+/**
+ * @brief Variable for the light state.
+ */
+int light_state = LIGHT_OFF;
+
+/**
+ * @brief Variable for the button state.
+ */
+int button_state = NOT_PUSHED;
+
+/**
+ * @brief Variable for the button last state.
+ */
+int button_last_state = NOT_PUSHED;
+
+/**
+ * @brief Variable for storing the time when the light start in milliseconds.
+ */
+unsigned long start_light_ms = 0;
+
+/**
+ * @brief Variable for storing the last push of the button.
+ */
+unsigned long last_push_ms = 0;
+
+/**
+ * @brief Sets up the used pins and the default variables settings.
+ */
 void setup()
 {
-  pinMode(buttonPin, INPUT);
-  pinMode(lightPin, OUTPUT);
-  digitalWrite(lightPin, LIGHT_OFF); // light off by default
+    // Used pins
+    pinMode(BUTTON_PIN, INPUT);
+    pinMode(LIGHT_PIN, OUTPUT);
+
+    // Defaults
+    digitalWrite(LIGHT_PIN, LIGHT_OFF);
+    light_state = LIGHT_OFF;
+    button_state = NOT_PUSHED;
+    button_last_state = NOT_PUSHED;
+    start_light_ms = 0;
+    last_push_ms = 0;
 }
 
+/**
+ * @brief Controls a button and a light to realize a dreaming light
+ * 
+ * Note: see the description of the project for more details.
+ */
 void loop()
 {
-  // read the button signal
-  buttonState = digitalRead(buttonPin);
+    // reads the button signal
+    button_state = digitalRead(BUTTON_PIN);
 
-  /* 
-   * Checks if the button is pushed and before it was not pushed
-   * with a margin of time (debounce) to avoid circuit flickering
-   */
+    /* 
+     * Checks if the button is pushed and before it was not pushed
+     * with a margin of time from the last operation
+     * to avoid circuit flickering.
+     */
   
-  if (
-    buttonState == PUSHED && buttonLastState == NOT_PUSHED &&
-    ((unsigned long) (millis() - lastPush)) > debounce
-  ) {
-
-    lastPush = millis(); // stores the last push time
-
-    /*
-     * If the light is ON and the button is pushed
-     * the light will be turned off.
-     * If the light is OFF and the button is pushed 
-     * the light will be turned on.
-     * In this last case the time is stored for a future use.
-     */
-
-    if      (lightState == LIGHT_ON)   { lightState = LIGHT_OFF; }
-    else if (lightState == LIGHT_OFF)  { lightState = LIGHT_ON; startLight = millis(); }
-    
-  } else {
-
-    /*
-     * The button is not pushed, if the light is on,
-     * checks if the working time is expired,
-     * if yes, turns off the light.
-     */
-    
     if (
-      lightState == LIGHT_ON &&
-      ((unsigned long) (millis() - startLight)) > lightTime
-    ) { lightState = LIGHT_OFF; }
-
-  }
-
-  digitalWrite(lightPin, lightState); // write the light state
-  buttonLastState = buttonState; // stores the last button state
+        button_state == PUSHED && button_last_state == NOT_PUSHED &&
+        ((unsigned long) (millis() - last_push_ms)) > DEBOUNCE_ms
+    ) {
+  
+        last_push_ms = millis(); // stores the last pushed button time
+    
+        /*
+         * If the light is ON and the button is pushed
+         * the light will be turned off.
+         * If the light is OFF and the button is pushed 
+         * the light will be turned on.
+         * In this last case the time is stored for a future use.
+         */
+    
+        if      (light_state == LIGHT_ON)  { light_state = LIGHT_OFF; }
+        else if (light_state == LIGHT_OFF) {
+            light_state = LIGHT_ON;
+            start_light_ms = millis();
+        }
+      
+    } else {
+  
+        /*
+         * The button is not pushed.
+         * If the light is on, checks if the working time is expired,
+         * if yes, turns off the light.
+         */
+        
+        if (
+            light_state == LIGHT_ON &&
+            ((unsigned long) (millis() - start_light_ms)) > LIGHT_TIME_ms
+        ) { light_state = LIGHT_OFF; }
+  
+    }
+  
+    digitalWrite(LIGHT_PIN, light_state); // writes the light state
+    button_last_state = button_state; // stores the last button state
   
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+# DreamingLight
+System for providing a light that turns on for a predefined time and then it turns off automatically.  
+Useful for the people are afraid about the night but that would be dream well thanks to the low light of the night. 
+ 
